d99836552def6ee5dd276c7369fb6146cb4fc20a
quiesscent <ciphedc@gmail.com>
2024-04-09 17:19:17 +0300

    Java 1

--- arrays.java
+++ arrays.java
@@ -1,6 +1,14 @@
 public class arrays{
 
 	public static void main(String[] args){
+		int[] numbers = {10,12,14,15};
+
+		System.out.println("Here are your numbers: ");
+
+		for (int i = 0; i < numbers.length; i++){
+			System.out.print(numbers[i] + ", ");
+		}
+		System.out.println(" ");
 		System.out.println("So Java works");
 	}
 }
